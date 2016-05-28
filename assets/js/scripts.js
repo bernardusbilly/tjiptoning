cf061cf657b4f898c36dc78110f32c40c02ade89
bernardusbilly <bernardus.billy@hotmail.com>
2016-05-28 16:44:07 -0700

    Add menu bar and finish with the animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 $(window).load(function() {
 	$('.loading-wrapper').hide();
+	$('.menu-container').addClass("animated fadeInRight");
 });
 
 $('.wrapper-project').hover(function() {
@@ -8,10 +9,27 @@ $('.wrapper-project').hover(function() {
 	$(this).find(".project-preview").css("opacity", "0");
 })
 
+$("ul>li>a").click(function(e) {
+	var target = $(this).attr("href");
+	e.preventDefault();
+	console.log(target);
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 1000, 'easeInOutQuint');
+});
+
 var service = $('.services').waypoint({
   handler: function() {
   	$('.services li').css("visibility", "visible");
     $('.services li').addClass("animated fadeInRight");
+  },
+  offset: "70%"
+})
+
+var yearIndicator = $('.year-indicator').waypoint({
+  handler: function(direction) {
+  	$(this.element).css("visibility", "visible");
+    $(this.element).addClass("animated fadeInDown");
   },
   offset: "70%"
 })
